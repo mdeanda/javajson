2b26b760471f9b3faf560754ccab9408ba522247
sodamnmad <sodamnmad@ad1a859c-b257-4791-a334-ad29afd51b78>
2006-04-13 05:37:04 +0000

    fixed empty array parsing

--- trunk/tests/net/sourceforge/javajson/test/TestParser.java
+++ trunk/tests/net/sourceforge/javajson/test/TestParser.java
@@ -11,18 +11,35 @@ public class TestParser extends TestCase {
 	public void testParseArray() throws Exception {
 		String input = "{\"array\":[1, 2, 3]}";
 		JsonObject json = JsonObject.parse(input);
-		
+
 		assertEquals(1, json.size());
 		assertTrue(json.isJsonArray("array"));
 		assertEquals(1, json.getJsonArray("array").getInt(0));
 		assertEquals(2, json.getJsonArray("array").getInt(1));
 		assertEquals(3, json.getJsonArray("array").getInt(2));
+
+		JsonArray arr;
+		try {
+			arr = JsonArray.parse("");
+			fail("Expected parse exception");
+		} catch (JsonException je) {
+
+		}
+
+		arr = JsonArray.parse("[]");
+		assertEquals(0, arr.size());
+
+		arr = JsonArray.parse("[1]");
+		assertEquals(1, arr.size());
+
+		arr = JsonArray.parse(" [ 3 ] ");
+		assertEquals(1, arr.size());
 	}
 
 	public void testParseNestedArray() throws Exception {
 		String input = "{\"array\":[1, [5, 6], 3]}";
 		JsonObject json = JsonObject.parse(input);
-		
+
 		assertEquals(1, json.size());
 		assertTrue(json.isJsonArray("array"));
 		assertEquals(1, json.getJsonArray("array").getInt(0));
@@ -36,7 +53,7 @@ public class TestParser extends TestCase {
 	public void testParseArrayNestedObject() throws Exception {
 		String input = "{\"array\":[1, {\"a\":5, \"b\" : 6}, 3]}";
 		JsonObject json = JsonObject.parse(input);
-		
+
 		assertEquals(1, json.size());
 		assertTrue(json.isJsonArray("array"));
 		assertEquals(1, json.getJsonArray("array").getInt(0));
@@ -47,31 +64,37 @@ public class TestParser extends TestCase {
 		assertEquals(3, json.getJsonArray("array").getInt(2));
 	}
 
-
 	public void testParseObjectNestedObject() throws Exception {
 		String input = "{\"obj1\":{ \"obj2\": {\"a\":5, \"b\" : 6}}}";
 		JsonObject json = JsonObject.parse(input);
-		
+
 		assertEquals(1, json.size());
 		assertTrue(json.isJsonObject("obj1"));
 		assertNotNull(json.getJsonObject("obj1").getJsonObject("obj2"));
-		assertEquals(5, json.getJsonObject("obj1").getJsonObject("obj2").getInt("a"));
-		assertEquals(6, json.getJsonObject("obj1").getJsonObject("obj2").getInt("b"));
-		
+		assertEquals(5, json.getJsonObject("obj1").getJsonObject("obj2")
+				.getInt("a"));
+		assertEquals(6, json.getJsonObject("obj1").getJsonObject("obj2")
+				.getInt("b"));
+
 		json = JsonObject.parse("{}");
 		json = JsonObject.parse("{ }");
 	}
 
 	public void testParseStrings() throws Exception {
-		assertEquals("value", JsonObject.parse("{\"key\": \"value\"}").getString("key"));
-		assertEquals("value", JsonObject.parse("{\"key\": 'value'}").getString("key"));
+		assertEquals("value", JsonObject.parse("{\"key\": \"value\"}")
+				.getString("key"));
+		assertEquals("value", JsonObject.parse("{\"key\": 'value'}").getString(
+				"key"));
 
-		assertEquals("val'ue", JsonObject.parse("{\"key\": \"val'ue\"}").getString("key"));
-		assertEquals("va\"lue", JsonObject.parse("{\"key\": 'va\"lue'}").getString("key"));
-		assertEquals("va\"lue", JsonObject.parse("{\"key\": \"va\\\"lue\"}").getString("key"));
-		assertEquals("va'lue", JsonObject.parse("{\"key\": 'va\\'lue'}").getString("key"));
+		assertEquals("val'ue", JsonObject.parse("{\"key\": \"val'ue\"}")
+				.getString("key"));
+		assertEquals("va\"lue", JsonObject.parse("{\"key\": 'va\"lue'}")
+				.getString("key"));
+		assertEquals("va\"lue", JsonObject.parse("{\"key\": \"va\\\"lue\"}")
+				.getString("key"));
+		assertEquals("va'lue", JsonObject.parse("{\"key\": 'va\\'lue'}")
+				.getString("key"));
 
-		
 		assertEquals("val\bue", JsonObject.parse("{\"key\": \"val\\bue\"}")
 				.getString("key"));
 		assertEquals("val\bue", JsonObject.parse("{\"key\": 'val\\bue'}")
@@ -100,38 +123,38 @@ public class TestParser extends TestCase {
 	}
 
 	public void testParseKeys() throws JsonException {
-		//simple keys, different quotes
+		// simple keys, different quotes
 		assertTrue(JsonObject.parse("{\"key\": 1}").hasKey("key"));
 		assertTrue(JsonObject.parse("{'key': 1}").hasKey("key"));
 		assertTrue(JsonObject.parse("{key: 1}").hasKey("key"));
-		
+
 		assertTrue(JsonObject.parse("{ke_y: 1}").hasKey("ke_y"));
 		assertTrue(JsonObject.parse("{ke3y: 1}").hasKey("ke3y"));
-		
+
 		try {
 			JsonObject.parse("{ke y: 1}");
 			fail("invalid key not caught");
 		} catch (JsonException ex) {
 		}
-		
+
 		try {
 			JsonObject.parse("{'key: 1}");
 			fail("invalid key not caught");
 		} catch (JsonException ex) {
 		}
-		
+
 		try {
 			JsonObject.parse("{key': 1}");
 			fail("invalid key not caught");
 		} catch (JsonException ex) {
 		}
-		
+
 		try {
 			JsonObject.parse("{0key: 1}");
 			fail("invalid key not caught");
 		} catch (JsonException ex) {
 		}
-		
+
 	}
 
 	public void testParseArrayAsRoot() throws JsonException {
