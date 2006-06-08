0c082d4e6c26cc16d889a03e5f4037ab3fd984dd
sodamnmad <sodamnmad@ad1a859c-b257-4791-a334-ad29afd51b78>
2006-06-08 03:26:54 +0000

    using text files for unicode since eclipse breaks them

--- trunk/tests/net/sourceforge/javajson/test/TestUnicode.java
+++ trunk/tests/net/sourceforge/javajson/test/TestUnicode.java
@@ -1,21 +1,28 @@
 package net.sourceforge.javajson.test;
 
-import net.sourceforge.javajson.JsonException;
+import java.io.InputStream;
+
 import net.sourceforge.javajson.JsonObject;
 import junit.framework.TestCase;
 
 public class TestUnicode extends TestCase {
+
+	private static final String PATH = "net/sourceforge/javajson/test/";
+	private static final String[] files = { "chinese_1", "japanese_1" };
 
 	public TestUnicode() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public void testChinese() throws Exception {
-		JsonObject json = JsonObject.parse("{text:???????????}");
-		assertEquals("???????????", json.getString("text"));
-
-		json = JsonObject.parse("{text:???????????????}");
-		assertEquals("???????????????", json.getString("text"));
-}
+	public void testUnicode() throws Exception {
+		for (int i = 0; i < files.length; i++) {
+			try {
+				InputStream is = TestUnicode.class.getResourceAsStream(PATH + files[i] + ".json");
+				JsonObject obj = JsonObject.parse(is);
+			} catch (Exception e) {
+				throw e;
+			}
+		}
+	}
 }
