939ecd49675ecb0da5b12c830f2df264b0412800
miguel de anda <mdeanda@users.noreply.github.com>
2009-04-05 06:47:28 +0000

    ignore hidden files

--- src/net/sourceforge/javajson/converter/Converter.java
+++ src/net/sourceforge/javajson/converter/Converter.java
@@ -9,19 +9,19 @@ import net.sourceforge.javajson.JsonArray;
 import net.sourceforge.javajson.JsonObject;
 
 /**
- * Converts java objects to json based on bean-style fields and classes/interfaces (to filter)
+ * Converts java objects to json based on bean-style fields and
+ * classes/interfaces (to filter)
  * 
  * @author miguel de anda
- * 
  */
 public class Converter {
 	protected final Logger log = Logger.getLogger(getClass());
 
 	public static final Converter instance = new Converter();
-	
+
 	private Converter() {
 	}
-	
+
 	public static Converter getInstance() {
 		return instance;
 	}
@@ -37,12 +37,12 @@ public class Converter {
 	}
 
 	/**
-	 * Convenience method for converting objects without a predefined
-	 * mapper. This method does not allow objects to be filtered in any
-	 * way and will inspect every public getter and return its value, 
-	 * if the value is not a Number, String or Date, it will then
-	 * do the conversion on that object as well. If the return type is
-	 * a Collection, it will be converted to a JsonArray
+	 * Convenience method for converting objects without a predefined mapper.
+	 * This method does not allow objects to be filtered in any way and will
+	 * inspect every public getter and return its value, if the value is not a
+	 * Number, String or Date, it will then do the conversion on that object as
+	 * well. If the return type is a Collection, it will be converted to a
+	 * JsonArray
 	 * 
 	 * @param o
 	 * @return
@@ -59,13 +59,39 @@ public class Converter {
 	}
 
 	/**
-	 * Convenience method for converting objects without a predefined
-	 * mapper. This method does not allow objects to be filtered in any
-	 * way and will inspect every public getter and return its value, 
-	 * if the value is not a Number, String or Date, it will be skipped.
+	 * Convenience method for converting objects without a predefined mapper.
+	 * This method does not allow objects to be filtered in any way and will
+	 * inspect every public getter and return its value, if the value is not a
+	 * Number, String or Date, it will then do the conversion on that object as
+	 * well. If the return type is a Collection, it will be converted to a
+	 * JsonArray
 	 * 
 	 * @param o
-	 * @param cls The class to read the fields from
+	 * @param flat
+	 *            Don't look at non basic fields (numbers, strings, dates,
+	 *            boolean)
+	 * @return
+	 * @throws SecurityException
+	 * @throws IllegalArgumentException
+	 * @throws NoSuchMethodException
+	 * @throws IllegalAccessException
+	 * @throws InvocationTargetException
+	 */
+	public JsonObject toJson(Object o, boolean flat) throws SecurityException,
+			IllegalArgumentException, NoSuchMethodException,
+			IllegalAccessException, InvocationTargetException {
+		return Mapper.DefaultMapper.toJson(o, flat);
+	}
+
+	/**
+	 * Convenience method for converting objects without a predefined mapper.
+	 * This method does not allow objects to be filtered in any way and will
+	 * inspect every public getter and return its value, if the value is not a
+	 * Number, String or Date, it will be skipped.
+	 * 
+	 * @param o
+	 * @param cls
+	 *            The class to read the fields from
 	 * @return
 	 * @throws SecurityException
 	 * @throws IllegalArgumentException
@@ -80,12 +106,12 @@ public class Converter {
 	}
 
 	/**
-	 * Convenience method for converting objects without a predefined
-	 * mapper. This method does not allow objects to be filtered in any
-	 * way and will inspect every public getter and return its value, 
-	 * if the value is not a Number, String or Date, it will then
-	 * do the conversion on that object as well. If the return type is
-	 * a Collection, it will be converted to a JsonArray
+	 * Convenience method for converting objects without a predefined mapper.
+	 * This method does not allow objects to be filtered in any way and will
+	 * inspect every public getter and return its value, if the value is not a
+	 * Number, String or Date, it will then do the conversion on that object as
+	 * well. If the return type is a Collection, it will be converted to a
+	 * JsonArray
 	 * 
 	 * @param o
 	 * @return
@@ -102,13 +128,14 @@ public class Converter {
 	}
 
 	/**
-	 * Convenience method for converting collections without a predefined
-	 * mapper. This method does not allow objects to be filtered in any
-	 * way and will inspect every public getter and return its value, 
-	 * if the value is not a Number, String or Date, it will be skipped.
+	 * Convenience method for converting objects without a predefined mapper.
+	 * This method does not allow objects to be filtered in any way and will
+	 * inspect every public getter and return its value, if the value is not a
+	 * Number, String or Date, it will then do the conversion on that object as
+	 * well. If the return type is a Collection, it will be converted to a
+	 * JsonArray
 	 * 
 	 * @param o
-	 * @param cls The class to read the fields from
 	 * @return
 	 * @throws SecurityException
 	 * @throws IllegalArgumentException
@@ -116,9 +143,55 @@ public class Converter {
 	 * @throws IllegalAccessException
 	 * @throws InvocationTargetException
 	 */
-	public JsonArray toJsonArray(Collection c, Class cls) throws SecurityException,
+	public JsonArray toJsonArray(Collection c, boolean flat) throws SecurityException,
 			IllegalArgumentException, NoSuchMethodException,
 			IllegalAccessException, InvocationTargetException {
+		return Mapper.DefaultMapper.toJsonArray(c, flat);
+	}
+
+	/**
+	 * Convenience method for converting collections without a predefined
+	 * mapper. This method does not allow objects to be filtered in any way and
+	 * will inspect every public getter and return its value, if the value is
+	 * not a Number, String or Date, it will be skipped.
+	 * 
+	 * @param o
+	 * @param cls
+	 *            The class to read the fields from
+	 * @return
+	 * @throws SecurityException
+	 * @throws IllegalArgumentException
+	 * @throws NoSuchMethodException
+	 * @throws IllegalAccessException
+	 * @throws InvocationTargetException
+	 */
+	public JsonArray toJsonArray(Collection c, Class cls)
+			throws SecurityException, IllegalArgumentException,
+			NoSuchMethodException, IllegalAccessException,
+			InvocationTargetException {
 		return Mapper.DefaultMapper.toJsonArray(c, cls);
+	}
+
+	/**
+	 * Convenience method for converting collections without a predefined
+	 * mapper. This method does not allow objects to be filtered in any way and
+	 * will inspect every public getter and return its value, if the value is
+	 * not a Number, String or Date, it will be skipped.
+	 * 
+	 * @param o
+	 * @param cls
+	 *            The class to read the fields from
+	 * @return
+	 * @throws SecurityException
+	 * @throws IllegalArgumentException
+	 * @throws NoSuchMethodException
+	 * @throws IllegalAccessException
+	 * @throws InvocationTargetException
+	 */
+	public JsonArray toJsonArray(Collection c, Class cls, boolean flat)
+			throws SecurityException, IllegalArgumentException,
+			NoSuchMethodException, IllegalAccessException,
+			InvocationTargetException {
+		return Mapper.DefaultMapper.toJsonArray(c, cls, flat);
 	}
 }
