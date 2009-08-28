beef91315f217f7d462e42d3c7f15c2b0ffc541c
sodamnmad <sodamnmad@ad1a859c-b257-4791-a334-ad29afd51b78>
2009-08-28 04:26:00 +0000

    deprecated getValueClass - getNativeType is faster and cleaner

--- trunk/src/net/sourceforge/javajson/JsonException.java
+++ trunk/src/net/sourceforge/javajson/JsonException.java
@@ -1,6 +1,8 @@
 package net.sourceforge.javajson;
 
 public class JsonException extends Exception {
+	private static final long serialVersionUID = -5846771141787676010L;
+
 	public JsonException(Throwable t) {
 		super(t);
 	}
