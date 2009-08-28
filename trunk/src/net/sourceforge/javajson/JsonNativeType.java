5bfa1b6bcc530d689494aade64f735a4cafaac32
sodamnmad <sodamnmad@ad1a859c-b257-4791-a334-ad29afd51b78>
2009-08-28 04:16:58 +0000

    remember type of value as set so we can to a proper tostring, this helps for other things as well but sandly forces us to remember more data making the value struct a bit larger.

---
+++ trunk/src/net/sourceforge/javajson/JsonNativeType.java
@@ -0,0 +1,5 @@
+package net.sourceforge.javajson;
+
+public enum JsonNativeType {
+	STRING, LONG, INTEGER, DOUBLE, FLOAT, JSON_OBJECT, JSON_ARRAY, BOOLEAN, NULL
+}
--- trunk/tests/net/sourceforge/javajson/test/TestNumbersAsStringOrNumber.java
+++ trunk/tests/net/sourceforge/javajson/test/TestNumbersAsStringOrNumber.java
@@ -5,7 +5,7 @@ import net.sourceforge.javajson.JsonObject;
 
 public class TestNumbersAsStringOrNumber extends TestCase {
 
-	public void testAsString() throws Exception {
+	public void testParseAsString() throws Exception {
 		JsonObject json = JsonObject.parse("{a:'32'}");
 		assertEquals("expected quotes around number", "{\"a\":\"32\"}", json
 				.toString());
@@ -23,7 +23,7 @@ public class TestNumbersAsStringOrNumber extends TestCase {
 				.toString());
 	}
 
-	public void testAsNumber() throws Exception {
+	public void testParseAsNumber() throws Exception {
 		JsonObject json = JsonObject.parse("{a:32}");
 		assertEquals("expected normal number", "{\"a\":32}", json.toString());
 
@@ -35,6 +35,18 @@ public class TestNumbersAsStringOrNumber extends TestCase {
 
 		json = JsonObject.parse("{a:-00.32}");
 		assertEquals("expected normal number", "{\"a\":-0.32}", json.toString());
+	}
+
+	public void testSetAsString() throws Exception {
+		JsonObject json = new JsonObject();
+		json.put("a", "007");
+		assertEquals("expected string", "{\"a\":\"007\"}", json.toString());
+	}
+
+	public void testSetAsNumber() throws Exception {
+		JsonObject json = new JsonObject();
+		json.put("a", 007);
+		assertEquals("expected number", "{\"a\":7}", json.toString());
 	}
 
 }
