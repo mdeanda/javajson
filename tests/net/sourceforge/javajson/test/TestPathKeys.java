8d2963f4b051e38126c196d7a4cba18cef196273
miguel de anda <mdeanda@users.noreply.github.com>
2010-06-10 01:57:42 +0000

    add method to get JsonValue object via varargs shortcut

---
+++ tests/net/sourceforge/javajson/test/TestPathKeys.java
@@ -0,0 +1,33 @@
+package net.sourceforge.javajson.test;
+
+import net.sourceforge.javajson.JsonArray;
+import net.sourceforge.javajson.JsonObject;
+
+import junit.framework.TestCase;
+
+public class TestPathKeys extends TestCase {
+
+	private JsonObject json;
+
+	private JsonArray array;
+
+	@Override
+	public void setUp() throws Exception {
+		json = JsonObject
+				.parse("{s:'string1',b:true,i:1,o:{s2:'string2',b2:true,i2:2,o2:{s3:'string3',b3:true,i3:3}}}");
+		array = new JsonArray();
+	}
+
+	public void testHasKey() throws Exception {
+		assertTrue("1 level deep", json.hasKey("b"));
+		assertTrue("2 levels deep", json.hasKey("o", "b2"));
+		assertTrue("3 levels deep", json.hasKey("o", "o2", "s3"));
+	}
+
+	public void testBoolean() throws Exception {
+		assertNotNull("1 level deep", json.get("b"));
+		assertNotNull("2 levels deep", json.get("o", "b2"));
+		assertNotNull("3 levels deep", json.get("o", "o2", "b3"));
+	}
+
+}
