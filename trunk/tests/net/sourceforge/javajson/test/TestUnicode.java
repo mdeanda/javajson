e7ea0d3fa4b1cd5442ddd8de9507fadaf4b4b53f
sodamnmad <sodamnmad@ad1a859c-b257-4791-a334-ad29afd51b78>
2012-01-03 09:09:18 +0000

    fixed unicode issues. it should parse non-english characters now.

--- trunk/tests/net/sourceforge/javajson/test/TestUnicode.java
+++ trunk/tests/net/sourceforge/javajson/test/TestUnicode.java
@@ -3,10 +3,11 @@ package net.sourceforge.javajson.test;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileReader;
-import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
-import net.sourceforge.javajson.JsonObject;
 import junit.framework.TestCase;
+import net.sourceforge.javajson.JsonObject;
 
 public class TestUnicode extends TestCase {
 
@@ -19,16 +20,26 @@ public class TestUnicode extends TestCase {
 		// TODO Auto-generated constructor stub
 	}
 
+	public void testUnicodeJavaStrings() throws Exception {
+		String uni = "\u747b\u7865\u3a74\ub522\ucec4\uccca\ud4e2\ub5da\ud6ea\ubbf7\ub8d8\u22b4\u007d";
+		String jsonstr = "{a:\"" + uni + "\"}";
+		JsonObject json = JsonObject.parse(jsonstr);
+		assertEquals(uni, json.getString("a"));
+		assertEquals(json.toString(), JsonObject.parse(json.toString()).toString());
+		//assertEquals(jsonstr, json.toString());
+	}
+
 	public void testUnicode() throws Exception {
 		for (int i = 0; i < files.length; i++) {
-				BufferedReader br = new BufferedReader(new FileReader(PATH
-						+ files[i] + ".txt"));
-				String expected = br.readLine();
+			BufferedReader br = new BufferedReader(new FileReader(PATH
+					+ files[i] + ".txt"));
+			String expected = br.readLine();
 
-				InputStream is = new FileInputStream(PATH + files[i] + ".json");
-				JsonObject obj = JsonObject.parse(is);
-				System.out.println(expected);
-				assertEquals(expected, obj.getString("text"));
+			Reader is = new InputStreamReader(new FileInputStream(PATH
+					+ files[i] + ".json"), "UTF-8");
+			JsonObject obj = JsonObject.parse(is);
+			System.out.println(expected);
+			assertEquals(expected, obj.getString("text"));
 		}
 	}
 }
