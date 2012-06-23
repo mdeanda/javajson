ad4aeb273815982f320d4e98dbfbd93a4b3c78ce
miguel de anda <mdeanda@users.noreply.github.com>
2012-06-23 00:09:11 +0000

    starting mapped converter

---
+++ src/net/sourceforge/javajson/mappedconverter/MappedConverter.java
@@ -0,0 +1,38 @@
+package net.sourceforge.javajson.mappedconverter;
+
+import java.util.Map;
+
+import net.sourceforge.javajson.JsonObject;
+import net.sourceforge.javajson.JsonValue;
+import net.sourceforge.javajson.converter.Reflection;
+
+public class MappedConverter {
+	private JsonObject map;
+
+	public MappedConverter(JsonObject map) {
+		this.map = map;
+	}
+
+	public JsonObject map(Object object, String mapKey) throws Exception {
+		JsonObject ret = null;
+		JsonObject map = this.map.getJsonObject(mapKey);
+		if (map != null) {
+			ret = new JsonObject();
+			// get all field values (fails b/c field names are incorrect, not
+			// using "field1" but "getField1"
+			Map<String, Object> flds = Reflection.getFields(object);
+			for (String key : map) {
+				JsonValue jv = map.get(key);
+				boolean accept = jv.getBoolean();
+
+				if (!accept)
+					continue;
+
+				if (flds.containsKey(key)) {
+					ret.put(key, flds.get(key));
+				}
+			}
+		}
+		return ret;
+	}
+}
