963d1686c7f61150ddd33633d55eed307b89ed15
miguel de anda <miguel@thedeanda.com>
2014-10-06 22:26:48 -0700

    test coverage

--- parser/src/test/java/com/thedeanda/javajson/test/TestAccumulate.java
+++ parser/src/test/java/com/thedeanda/javajson/test/TestAccumulate.java
@@ -32,6 +32,19 @@ public class TestAccumulate {
 	}
 
 	@Test
+	public void testBoolean2() {
+		json.put(key, "");
+		json.accumulate(key, true);
+		json.accumulate(key, false);
+
+		JsonArray a = json.getJsonArray(key);
+		assertNotNull(a);
+		assertEquals("", a.getString(0));
+		assertTrue(a.getBoolean(1));
+		assertFalse(a.getBoolean(2));
+	}
+
+	@Test
 	public void testInt() {
 		json.accumulate(key, 4);
 		json.accumulate(key, 2);
@@ -43,6 +56,19 @@ public class TestAccumulate {
 	}
 
 	@Test
+	public void testInt2() {
+		json.put(key, "");
+		json.accumulate(key, 4);
+		json.accumulate(key, 2);
+
+		JsonArray a = json.getJsonArray(key);
+		assertNotNull(a);
+		assertEquals("", a.getString(0));
+		assertEquals(4, a.getInt(1));
+		assertEquals(2, a.getInt(2));
+	}
+
+	@Test
 	public void testString() {
 		json.accumulate(key, "a");
 		json.accumulate(key, "b");
@@ -51,6 +77,19 @@ public class TestAccumulate {
 		assertNotNull(a);
 		assertEquals("a", a.getString(0));
 		assertEquals("b", a.getString(1));
+	}
+
+	@Test
+	public void testString2() {
+		json.put(key, true);
+		json.accumulate(key, "a");
+		json.accumulate(key, "b");
+
+		JsonArray a = json.getJsonArray(key);
+		assertNotNull(a);
+		assertTrue(a.getBoolean(0));
+		assertEquals("a", a.getString(1));
+		assertEquals("b", a.getString(2));
 	}
 
 	@Test
@@ -67,6 +106,21 @@ public class TestAccumulate {
 	}
 
 	@Test
+	public void testDouble2() {
+		json.put(key, "");
+		double d1 = 3.14;
+		double d2 = 2.72;
+		json.accumulate(key, d1);
+		json.accumulate(key, d2);
+
+		JsonArray a = json.getJsonArray(key);
+		assertNotNull(a);
+		assertEquals("", a.getString(0));
+		assertTrue(d1 == a.getDouble(1));
+		assertTrue(d2 == a.getDouble(2));
+	}
+
+	@Test
 	public void testFloat() {
 		float d1 = 3.14f;
 		float d2 = 2.72f;
@@ -78,12 +132,27 @@ public class TestAccumulate {
 		assertTrue(d1 == a.getFloat(0));
 		assertTrue(d2 == a.getFloat(1));
 	}
-	
+
+	@Test
+	public void testFloat2() {
+		json.put(key, "");
+		float d1 = 3.14f;
+		float d2 = 2.72f;
+		json.accumulate(key, d1);
+		json.accumulate(key, d2);
+
+		JsonArray a = json.getJsonArray(key);
+		assertNotNull(a);
+		assertEquals("", a.getString(0));
+		assertTrue(d1 == a.getFloat(1));
+		assertTrue(d2 == a.getFloat(2));
+	}
+
 	@Test
 	public void testArray() {
 		JsonArray a1 = new JsonArray();
 		JsonArray a2 = new JsonArray();
-		
+
 		json.accumulate(key, a1);
 		json.accumulate(key, a2);
 
@@ -92,13 +161,28 @@ public class TestAccumulate {
 		assertEquals(a1, a.getJsonArray(0));
 		assertEquals(a2, a.getJsonArray(1));
 	}
-	
-	
+
+	@Test
+	public void testArray2() {
+		json.put(key, "");
+		JsonArray a1 = new JsonArray();
+		JsonArray a2 = new JsonArray();
+
+		json.accumulate(key, a1);
+		json.accumulate(key, a2);
+
+		JsonArray a = json.getJsonArray(key);
+		assertNotNull(a);
+		assertEquals("", a.getString(0));
+		assertEquals(a1, a.getJsonArray(1));
+		assertEquals(a2, a.getJsonArray(2));
+	}
+
 	@Test
 	public void testObject() {
 		JsonObject a1 = new JsonObject();
 		JsonObject a2 = new JsonObject();
-		
+
 		json.accumulate(key, a1);
 		json.accumulate(key, a2);
 
@@ -106,5 +190,21 @@ public class TestAccumulate {
 		assertNotNull(a);
 		assertEquals(a1, a.getJsonObject(0));
 		assertEquals(a2, a.getJsonObject(1));
+	}
+
+	@Test
+	public void testObject2() {
+		json.put(key, "");
+		JsonObject a1 = new JsonObject();
+		JsonObject a2 = new JsonObject();
+
+		json.accumulate(key, a1);
+		json.accumulate(key, a2);
+
+		JsonArray a = json.getJsonArray(key);
+		assertNotNull(a);
+		assertEquals("", a.getString(0));
+		assertEquals(a1, a.getJsonObject(1));
+		assertEquals(a2, a.getJsonObject(2));
 	}
 }
